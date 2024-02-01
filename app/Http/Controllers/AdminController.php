0163e4d3526988434b93dada721ad13c35f1a075
Fahmi Noh <fahminoh.dev@gmail.com>
2024-02-01 17:22:46 +0800

    admindashboard_01022024

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function AdminDashboard(){
+
+        return view('admin.index');
+    } 
+
+    // public function AdminLogout(Request $request){
+    //     Auth::guard('web')->logout();
+
+    //     $request->session()->invalidate();
+
+    //     $request->session()->regenerateToken();
+
+    //     return redirect('/admin/login');
+    // }
+
+    public function AdminLogin(){
+        return view('admin.admin_login');
+    } 
+
+    // public function AdminProfile(){
+    //     $id = Auth::user()->id;
+    //     $profileData = User::find($id);
+    //     return view('admin.admin_profile_view', compact('profileData'));
+
+    // } 
+
+    // public function AdminProfileStore(Request $request){
+        
+    //     $id = Auth::user()->id;
+    //     $data = User::find($id);
+    //     $data->name = $request->name;
+    //     $data->username = $request->username;
+    //     $data->email = $request->email;
+    //     $data->phone = $request->phone;
+    //     $data->address = $request->address;
+
+    //     if ($request->file('photo')) {
+            
+    //         $file = $request->file('photo');
+    //         @unlink(public_path('upload/admin_images/'.$data->photo));
+    //         $filename = date('YmdHi').$file->getClientOriginalName();
+    //         $file->move(public_path('upload/admin_images'),$filename);
+    //         $data['photo'] = $filename;
+
+    //     }
+
+    //     $data->save();
+
+    //     $notification = array(
+    //         'message' => 'Admin Profile Updated Successfully',
+    //         'alert-type' => 'success'
+    //     );
+
+    //     return redirect()->back()->with($notification);
+    // } 
+
+    // public function AdminChangePassword(){
+
+    //     $id = Auth::user()->id;
+    //     $profileData = User::find($id);
+    //     return view('admin.admin_change_password', compact('profileData'));
+    // }
+
+    // public function AdminPasswordUpdate(Request $request){
+        
+    //     //Password Validation
+    //     $request->validate([
+    //         'old_password' => 'required',
+    //         'new_password' => 'required|confirmed'
+    //     ]);
+
+    //     //Old password match validation
+    //     if(!Hash::check($request->old_password, auth::user()->password)){
+    //         $notification = array(
+    //             'message' => 'Old Password Does Not Match!',
+    //             'alert-type' => 'error'
+    //         );
+    //         return back()->with($notification);
+    //     }
+
+    //     //Update new password
+    //     User::whereId(auth::user()->id)->update([
+    //         'password' => Hash::make($request->new_password)
+    //     ]);
+
+    //     $notification = array(
+    //         'message' => 'Password updated successfully!',
+    //         'alert-type' => 'success'
+    //     );
+    //     return back()->with($notification);
+    // } 
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +30,24 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//ADMIN Group Middleware
+Route::middleware(['auth','roles:admin'])->group(function(){
+
+    //redirect to admin dashboard
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    //admin logout
+    // Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    //page route
+    // Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    // Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    //data route
+    // Route::POST('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    // Route::POST('/admin/update/password', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
+    
+    
+    
+    }); //End of ADMIN group middleware
+
+    //adminLogin
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
